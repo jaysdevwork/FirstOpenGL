a08f80d9aa5fc765ebd607c4d6030fb7b4298e92
jaylon <123032130+codeDuck1@users.noreply.github.com>
2025-05-13 23:44:51 -0400

    2 shader program exercise

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -19,13 +19,27 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
 
+const char* fragmentShaderSource2 = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
 
 // three vertices for triange
 float vertices[] =
 {
     -0.5f, -0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
+    0.0f, 0.5f, 0.0f,
+};
+
+float vertices2[] =
+{
+    -0.9f, -0.9f, 0.0f,
+    0.1f, -0.9f, 0.0f,
+    -0.4f, 0.1f, 0.0f
 };
 
 // unique vertices of rectangle
@@ -83,6 +97,8 @@ int main()
     // Make context of window the main context on current thread
     glfwMakeContextCurrent(window);
 
+    //glfwSwapInterval(0); UNCOMMENT TO DISABLE V SYNC
+
     // register function for window resize
     // IMPORTANT: register callback functions after created window and before render loop is initiated
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -115,6 +131,40 @@ int main()
     // copy vertex data into currently bound buffer memory
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // tells opengl how to link vertex data to the vertex shader's shader attributes
+    // vbo bound to gl_array_buffer, so vertex attribute 0 now assoc with its vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 1st param is location in vertex shader to pass to
+    glEnableVertexAttribArray(0);
+
+
+
+
+    // element buffer object for effecient use of vertices
+    // DEFINES DRAWING ORDER OF VERTICES IN VBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+
+
+
+    // VAO AND VBO FOR SECOND TRIANGLE. 
+    //glBindVertexArray(0); // unbind prev vao. not necessary but helps with readability- jay
+    unsigned int VAO2;
+    glGenVertexArrays(1, &VAO2); // can also store vaos as array with 1st param as num of vaos
+    glBindVertexArray(VAO2);
+
+    unsigned int VBO2;
+    glGenBuffers(1, &VBO2); // generate buffer id
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 1st param is location in vertex shader to pass to
+    glEnableVertexAttribArray(0);
+    // END
+
+
 
     // create vertex shader object 
     unsigned int vertexShader;
@@ -139,15 +189,30 @@ int main()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     // dont need shader objects once linked to program
-    glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // specify how opengl should interpret vertex data
-    // tells opengl how to link vertex data to the vertex shader's shader attributes
-    // vbo bound to gl_array_buffer, so vertex attribute 0 now assoc with its vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 1st param is location in vertex shader to pass to
-    glEnableVertexAttribArray(0);
+    // 2nd shader program for 2nd triangle
+    // create fragment shader with yellow color
+    unsigned int fragmentShader2;
+    fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    // attach source code to shader object and compile
+    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+    glCompileShader(fragmentShader2);
+    // create another shader program
+    unsigned int shaderProgram2;
+    shaderProgram2 = glCreateProgram();
+    // attach compiled shaders to program obj and link them
+    glAttachShader(shaderProgram2, vertexShader);
+    glAttachShader(shaderProgram2, fragmentShader2);
+    glLinkProgram(shaderProgram2);
 
+    glDeleteShader(fragmentShader2);
+    glDeleteShader(vertexShader);
+
+
+
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // to enable wireframe mode
     // render loop!- iteration of render loop called a FRAME
     // so app keeps drawing images and handle input until told to stop
     while (!glfwWindowShouldClose(window)) // checks if glfw has been told to close
@@ -164,8 +229,17 @@ int main()
         // object and thus the shaders
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); 
+
+        // replace to render from index buffer vs array buffer
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // takes indices from ebo bound to gl_elem_array_buff target
+        //glBindVertexArray(0); // UNBIND CURRENTLY BOUND VAO
+
+        // draw second triangle, using shader program with yellow frag shader
+        glUseProgram(shaderProgram2);
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         // CHECK AND CALL EVENTS AND SWAP BUFFERS:
         // swap color buffer used torender and show as ouput to screen
